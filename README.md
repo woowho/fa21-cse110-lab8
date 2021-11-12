fc22b927af6746fa0a354ced369b8e38e0887aa1
wu.alex <9862571+WuEtAl@users.noreply.github.com>
2021-11-11 17:12:19 -0800

    Finished project

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Lab 8 - Starter
+
+NO PARTNERS.
+
+## Answer to Questions
+
+
+### Q1
+
+In your own words: Where would you fit your automated tests in your Recipe project development pipeline?
+
+    All three are needed. We want to keep code in repo as clean as possible. Local testing speeds up production, remote testing forces good software engineering pratices and keeps codebase clean. Final testing ensures stability and usability of the product.
+
+### Q2
+
+Would you use an end to end test to check if a function is returning the correct output?
+
+    No. That is for unit testing.
+
+### Q3
+
+Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+    This is not a good feature to test because "message" is not a "unit." It consists of writing, sending, and receiving, and combining all three at once breaks the idea of a unit test.
+
+### Q4
+
+Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+    This is good to test for unit tests because it is an atomic feature and can be tested easily.
+
+
+## Screenshut
+
+![Screenshot](screenshot.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,55 @@
 // unit.test.js
 
+const { test, expect } = require('@jest/globals');
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// Phone Number
+
+test("Phone# with/out punctuations", () => expect(functions.isPhoneNumber('123-456-7890')).toBe(true));
+
+test("Phone# with punctuations", () => expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true));
+
+test("Phone# wrong length", () => expect(functions.isPhoneNumber('555-555-555')).toBe(false));
+
+test("Phone# with letters", () => expect(functions.isPhoneNumber('1-800-ATTORNEY')).toBe(false));
+
+
+// Email
+
+test("Email: generic", () => expect(functions.isEmail('biden@whitehouse.gov')).toBe(true));
+
+test("Email: special char", () => expect(functions.isEmail('biden9joe@whitehouse.gov')).toBe(true));
+
+test("Email: double @", () => expect(functions.isEmail('biden@home@whitehouse.gov')).toBe(false));
+
+test("Email: wrong domain", () => expect(functions.isEmail('biden@whitehouse')).toBe(false));
+
+// isStrongPassword
+
+test("Password: long", () => expect(functions.isStrongPassword('aBBSD1231_asdef')).toBe(true));
+
+test("Password: normal", () => expect(functions.isStrongPassword('aBBS231asd')).toBe(true));
+
+test("Password: too long", () => expect(functions.isStrongPassword('aBBSD1231231_asdaBBSD1231231_asd')).toBe(false));
+
+test("Password: not begin with letter", () => expect(functions.isStrongPassword('1abcasdjlkjalsdk')).toBe(false));
+
+// isDate
+
+test("Date: normal", () => expect(functions.isDate('7/29/1969')).toBe(true));
+
+test("Date: double month", () => expect(functions.isDate('02/31/1999')).toBe(true));
+
+test("Date: letter", () => expect(functions.isDate('02/x1/1999')).toBe(false));
+
+test("Date: 2 digit year", () => expect(functions.isDate('02/31/99')).toBe(false));
+
+// isHexColor
+
+test("HexColor: 3", () => expect(functions.isHexColor('FFF')).toBe(true));
+
+test("HexColor: 6", () => expect(functions.isHexColor('FFFFFF')).toBe(true));
+
+test("HexColor: X", () => expect(functions.isHexColor('AFX')).toBe(false));
+
+test("HexColor: too long", () => expect(functions.isHexColor('FFFFFFF')).toBe(false));
